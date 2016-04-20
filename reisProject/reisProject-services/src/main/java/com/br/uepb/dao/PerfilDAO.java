bb4ba53a34952a42f0284d3d99a9c3efcf98bfba
melqui-andrade <melqui.inbox@gmail.com>
2016-04-20 12:04:46 -0300

    Métodos de CRUD de perfil implementados na classe PerfilDAO

--- reisProject/reisProject-services/src/main/java/com/br/uepb/dao/PerfilDAO.java
+++ reisProject/reisProject-services/src/main/java/com/br/uepb/dao/PerfilDAO.java
@@ -1,5 +1,68 @@
 package com.br.uepb.dao;
 
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.br.uepb.model.PerfilDomain;
+
+import conexaoBD.HibernateUtil;
+
 public class PerfilDAO {
+	
+	private Session sessaoAtual;
+	
+	public PerfilDAO(){
+		
+	}
+	
+	/**
+	 * Salva ou atualiza perfil
+	 * @param perfil Sem o ID: Salvar, com ID: atualiza
+	 */
+	public void salvaPerfil(PerfilDomain perfil){
+		if(ehNovoUsuario(perfil)){
+			SessaoAtual().save(perfil);
+		}
+		else{
+			SessaoAtual().update(perfil);
+		}
+		SessaoAtual().close();
+	}
+	
+	public void excluiPerfil(PerfilDomain perfil){
+		SessaoAtual().delete(perfil);
+		SessaoAtual().close();
+	}
+	
+	public PerfilDomain obtemPerfil(int idPerfil){
+		PerfilDomain perfil = (PerfilDomain)SessaoAtual().get(PerfilDomain.class, idPerfil);
+		SessaoAtual().close();
+		return perfil;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<PerfilDomain> listaPerfis(){
+		List<PerfilDomain> perfis = 
+				(List<PerfilDomain>)SessaoAtual().createQuery("select * from perfil").list();
+		SessaoAtual().close();
+		return perfis;
+	}
+	
+	private Session SessaoAtual(){
+		if (sessaoAtual == null || !sessaoAtual.isOpen()){
+			sessaoAtual = HibernateUtil.getSessionFactory().openSession();
+		}
+		return sessaoAtual;
+	}
+	
+	private boolean ehNovoUsuario(PerfilDomain perfil){
+		if(perfil.getId() > 0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 
 }
