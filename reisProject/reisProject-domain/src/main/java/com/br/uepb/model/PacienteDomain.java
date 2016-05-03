1524c7111c9661eb97be9dee3ac900521390d21e
melqui-andrade <melqui.inbox@gmail.com>
2016-05-03 17:45:28 -0300

    Ajustes no mapeamento das classes MedicaoBalancaDomain, MedicaoOximetroDomain e PacienteDomain

--- reisProject/reisProject-domain/src/main/java/com/br/uepb/model/PacienteDomain.java
+++ reisProject/reisProject-domain/src/main/java/com/br/uepb/model/PacienteDomain.java
@@ -2,19 +2,10 @@ package com.br.uepb.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "paciente")
