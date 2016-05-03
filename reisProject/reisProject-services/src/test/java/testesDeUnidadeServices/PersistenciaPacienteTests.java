d5e62456480d6a38da337b31f252e4b6aeac2204
melqui-andrade <melqui.inbox@gmail.com>
2016-05-03 19:46:07 -0300

    PersistenciaPacienteTests adaptado para suportar as alterações de PacienteDAO

--- reisProject/reisProject-services/src/test/java/testesDeUnidadeServices/PersistenciaPacienteTests.java
+++ reisProject/reisProject-services/src/test/java/testesDeUnidadeServices/PersistenciaPacienteTests.java
@@ -1,37 +1,26 @@
 package testesDeUnidadeServices;
 
 import static junit.framework.Assert.assertTrue;
-import org.junit.Before;
 import org.junit.Test;
 import com.br.uepb.dao.PacienteDAO;
-import com.br.uepb.dao.LoginDAO;
 import com.br.uepb.model.PacienteDomain;
 
 public class PersistenciaPacienteTests {
 
-	@Before
-	public void criarPerfil(){
-		LoginDAO perfilDAO = new LoginDAO();
-		PacienteDomain paciente = new PacienteDomain();
 		
-		//perfilDAO.salvaLogin(paciente);
-	}
-	
 	@Test
 	public void criarPacienteTest() {
 		PacienteDAO pacienteDAO = new PacienteDAO();
-		LoginDAO perfilDAO = new LoginDAO();
 		PacienteDomain novoPaciente = new PacienteDomain();
-		/*
-		PerfilDomain perfil = perfilDAO.obtemPerfil(1);
-		
-		novoPaciente.setLogin("joao");
-		novoPaciente.setSenha("senha123");
+				
 		novoPaciente.setNome("João Lopes");
-		novoPaciente.setPerfil(perfil);
+		novoPaciente.setEndereco("Rua Coronel Falamansa");
+		novoPaciente.setCidade("Arco Verde");
+		novoPaciente.setSexo("M");
+		novoPaciente.setTelefoneCasa("8335640298");
 		
 		pacienteDAO.salvaPaciente(novoPaciente);
-		*/
+		
 		assertTrue(novoPaciente.getId() > 0);
 	}
 
