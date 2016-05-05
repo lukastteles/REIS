98dd0ddfb321728456e633539ac0a88a569ebe24
FabinhoDias <fabio.ds@live.com>
2016-05-05 15:20:29 -0300

    Classe em manutenção do HL7

--- reisProject/reisProject-business/src/main/java/com/br/uepb/hl7/tranformacaoDaStringParaHL7.java
+++ reisProject/reisProject-business/src/main/java/com/br/uepb/hl7/tranformacaoDaStringParaHL7.java
@@ -1,15 +1,43 @@
 package com.br.uepb.hl7;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+
+import com.br.uepb.model.MedicaoOximetroDomain;
 import com.br.uepb.model.PacienteDomain;
 
 import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.app.Connection;
+import ca.uhn.hl7v2.app.ConnectionHub;
+import ca.uhn.hl7v2.app.HL7Service;
+import ca.uhn.hl7v2.app.ServerConfiguration;
+import ca.uhn.hl7v2.conf.check.Validator;
+import ca.uhn.hl7v2.conf.store.CodeStoreRegistry;
+import ca.uhn.hl7v2.conf.store.ProfileStore;
+import ca.uhn.hl7v2.llp.LowerLayerProtocol;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v22.datatype.PN;
 import ca.uhn.hl7v2.model.v22.message.ADT_A01;
+import ca.uhn.hl7v2.model.v22.message.ORU_R01;
+import ca.uhn.hl7v2.model.v22.segment.MSH;
+import ca.uhn.hl7v2.model.v22.segment.NK1;
+import ca.uhn.hl7v2.model.v22.segment.OBR;
+import ca.uhn.hl7v2.model.v22.segment.OBX;
+import ca.uhn.hl7v2.model.v22.segment.PID;
+import ca.uhn.hl7v2.model.v23.message.MDM_T02;
 import ca.uhn.hl7v2.parser.EncodingNotSupportedException;
+import ca.uhn.hl7v2.parser.GenericParser;
+import ca.uhn.hl7v2.parser.ModelClassFactory;
 import ca.uhn.hl7v2.parser.Parser;
+import ca.uhn.hl7v2.parser.ParserConfiguration;
+import ca.uhn.hl7v2.parser.PipeParser;
+import ca.uhn.hl7v2.parser.XMLParser;
+import ca.uhn.hl7v2.util.SocketFactory;
+import ca.uhn.hl7v2.validation.ValidationContext;
+import ca.uhn.hl7v2.validation.ValidationExceptionHandlerFactory;
+import ca.uhn.hl7v2.validation.builder.ValidationRuleBuilder;
 
 public class tranformacaoDaStringParaHL7 {
 
@@ -26,7 +54,7 @@ public class tranformacaoDaStringParaHL7 {
 
 	private String msg2 = "MSH|^~\\&|VSM001|MIRTH_CONNECT|HIS001|MIRTH_CONNECT|20100511220525||ORU ^R01|MSG0000001|P|2.5|||NE|NE|CO|8859/1|ES-CO"
 			+ "PID||6537077|6537077^^^^CC||ANDRES FELIPE^FERNANDEZ CORTES||19860705|M"
-			+ "OBR|1||VS12340000|28562-7^Vital Signs^LN"
+			//+ "OBR|1||VS12340000|28562-7^Vital Signs^LN"
 			+ "OBX|1|NM|271649006^Systolic blood pressure^SNOMED-CT||132|mm[Hg]|90-120|H|||F|||20100511220525"
 			+ "OBX|2|NM|271650006^Diastolic blood pressure^SNOMED-CT||86|mm[Hg]|60-80|H|||F|||20100511220525"
 			+ "OBX|3|NM|6797001^Mean blood pressure^SNOMED-CT||94|mm[Hg]|92-96|N|||F|||20100511220525"
@@ -35,7 +63,7 @@ public class tranformacaoDaStringParaHL7 {
 			+ "OBX|6|NM|431314004^SpO2^SNOMED-CT||90|%|94-100|L|||F|||20100511220525";
 
 	public tranformacaoDaStringParaHL7() {
-		HapiContext context = new DefaultHapiContext();
+		/*HapiContext context = new DefaultHapiContext();
 
 		Parser p = context.getGenericParser();
 
@@ -51,17 +79,62 @@ public class tranformacaoDaStringParaHL7 {
 			return;
 		}
 
-		adtMsg = (ADT_A01) hapiMsg;
+		adtMsg = (ADT_A01) hapiMsg;*/
 
 	}
+	
+	public String Oximetro(MedicaoOximetroDomain medicaoOximetroDomain) throws HL7Exception, IOException {
+		MDM_T02 mdm = new MDM_T02();
+		mdm.initQuickstart("MDM", "T02", "M");
+		
+		ca.uhn.hl7v2.model.v23.segment.MSH msh = mdm.getMSH();
+		
+		
+		
+		ca.uhn.hl7v2.model.v23.segment.OBX obx = mdm.getOBX();
+		obx.getObservationIdentifier().getIdentifier().setValue("1");
+		
+		
+		
+		String msg = "ads";
+		return msg;
+	}
+	
+	@SuppressWarnings("resource")
+	public String criarMessage(PacienteDomain pacienteDomain) throws HL7Exception, IOException {
+		
+		ADT_A01 adt = new ADT_A01();
+		adt.initQuickstart("ADT", "A01", "P");
+		
+		MSH msh = adt.getMSH();
+		msh.getSendingApplication().setValue("Teste");
+		msh.getSequenceNumber().setValue("123");
+		
+		String idPessoa = String.valueOf(pacienteDomain.getId());
+		PID pid = adt.getPID();
+		pid.getPatientName().getGivenName().setValue(pacienteDomain.getNome());
+		//pid.getPid10_Race().setValue(idPessoa);
+		pid.getSex().setValue(pacienteDomain.getSexo());
+		
+		NK1 nk1 = adt.getNK1();
+		nk1.getAddress().getCity().setValue(pacienteDomain.getCidade());
+		nk1.getBusinessPhoneNumber().setValue(pacienteDomain.getTelefoneCasa());
+		
+		HapiContext context = new DefaultHapiContext();
+		Parser parser = context.getPipeParser();
+		
+		String mensagem = parser.encode(adt);
+		return mensagem;
+	}
 
+	/*
 	public PacienteDomain informacoesDoPaciente() {
 		PacienteDomain paciente = new PacienteDomain();
 
 		return paciente;
 	}
-
-	public static void main(String[] args) {
+	 */
+	public static void main(String[] args) throws HL7Exception, IOException {
 
 		tranformacaoDaStringParaHL7 tranformacaoDaStringParaHL7 = new tranformacaoDaStringParaHL7();
 
@@ -81,11 +154,28 @@ public class tranformacaoDaStringParaHL7 {
 		 * PN Ã© um tipo de dados HL7 constituÃ­do por vÃ¡rios componentes, tais
 		 * como nome de famÃ­lia, nome, etc.
 		 */
-		PN patientName = adtMsg.getPID().getPatientName();
+		//PN patientName = adtMsg.getPID().getPatientName();
 		//AD endereco = adtMsg.getNK1().getAddress();
 		//TN telefone = adtMsg.getNK1().getBusinessPhoneNumber();
-
+		
+		PacienteDomain domain = new PacienteDomain();
+		domain.setId(1);
+		domain.setNome("Bruno Teles Janaina Einstein");
+		domain.setCidade("Campina Grande");
+		domain.setSexo("Masculino");
+		domain.setTelefoneCasa("(83) 99999-9999");
+		
+		String msg6 = tranformacaoDaStringParaHL7.criarMessage(domain);
+		System.out.println();
+		System.out.println(msg6);
+/*
 		String familyName = patientName.getFamilyName().getValue();
+		boolean a = adtMsg.getParser().equals(domain);
+		if(a) {
+			System.out.println("certo");
+		} else {
+			System.out.println("errado");
+		}
 		System.out.println(familyName);
 		System.out.println(patientName.getGivenName().getValue());
 		System.out.println(patientName.getMiddleInitialOrName().getValue());
@@ -96,7 +186,7 @@ public class tranformacaoDaStringParaHL7 {
 		//System.out.println(endereco.getCountry().getValue());
 
 		//System.out.println("Telefone:");
-		//System.out.println(telefone.getValue());
+		//System.out.println(telefone.getValue()); */
 	}
 
 }
