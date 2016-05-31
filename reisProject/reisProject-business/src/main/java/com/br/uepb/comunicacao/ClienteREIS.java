d4560995e0b8db978aa9a8fa16eca5b160ddf210
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2016-05-31 14:51:05 -0300

    HL7

--- reisProject/reisProject-business/src/main/java/com/br/uepb/comunicacao/ClienteREIS.java
+++ reisProject/reisProject-business/src/main/java/com/br/uepb/comunicacao/ClienteREIS.java
@@ -1,28 +1,52 @@
 package com.br.uepb.comunicacao;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ClienteREIS {
 
-	public static void main(String[] args) throws Exception {
-		 Socket cliente = new Socket("192.168.1.72", 345);
+	private String ip;
+	private int porta;
+	private String mensagem; 
+	
+	public void enviarMensagem(String ip, int porta, String mensagem) throws Exception {
+		 Socket cliente = new Socket(ip, porta);
 	     System.out.println("O cliente se conectou ao servidor!");
-	     
-	     Scanner teclado = new Scanner(System.in);
 	     PrintStream saida = new PrintStream(cliente.getOutputStream());
 	     
-	     while (teclado.hasNextLine()) {
-	       saida.println(teclado.nextLine());
-	     }
-	     
+	     saida.println(mensagem);
+	     cliente.getInputStream();
 	     saida.close();
-	     teclado.close();		
+	     
+	}
+	
+	public void fecharConexao(Socket so) throws IOException {
+		so.close();
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public int getPorta() {
+		return porta;
+	}
+
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
 	}
 	
 }	
